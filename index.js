bbe2d8ea1c61033df83fae17996ebc287716dc6d
Deny <denyburgos1305@gmail.com>
2023-07-03 16:29:07 -0400

    se agrega el api del clima

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const input = document.querySelector("#input");
 const informacion = document.querySelector("#paises-container");
 let countries = [];
-
 const getIp = async () => {
     try {
         const response = await (await fetch(`https://restcountries.com/v3.1/all`)).json();
@@ -11,44 +10,49 @@ const getIp = async () => {
     }
 } 
 getIp();
-const getWeather = async () => {
+
+const getWeather = async (lat, lon) => {
     try {
-        const weatherResponse =await (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=75ef195357bb4c86442cecf9f177705d`)).json();
-        console.log(weatherResponse);
+        const weatherResponse = await (await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=75ef195357bb4c86442cecf9f177705d&lang={es}`)).json();
+        const clima = weatherResponse.weather[0].description;
+        return clima
     } catch (error) {
         alert('error')
     }
 }
-getWeather();
 
 input.addEventListener("input", e =>{
     e.preventDefault();
     const filteredCountries = countries.filter(country => country.name.common.toLowerCase().startsWith(input.value.toLowerCase()));
-        informacion.innerHTML = '';
-        if (filteredCountries.length > 10 && filteredCountries.length < 200) {
-            informacion.innerHTML =  `<h3>Debes ser mas especifico</h3> `
-        }
-        if (filteredCountries.length < 10) {
-            filteredCountries.forEach(index => {
-                informacion.innerHTML +=
-                `
-                <div class="paises-info">
-                <img class = "pais-img" src="${index.flags.svg}" alt="">
-                <h3>${index.name.common}</h3>
-                </div>
-                `
-            });
-        }
-        if (filteredCountries.length === 1) {
-            informacion.innerHTML =  `
+    informacion.innerHTML = '';
+    console.log(filteredCountries);
+    if (filteredCountries.length > 10 && filteredCountries.length < 200) {
+        informacion.innerHTML =  `<h3>Debes ser mas especifico</h3> `
+    }
+    if (filteredCountries.length < 10) {
+        filteredCountries.forEach(index => {
+            informacion.innerHTML +=
+            `
             <div class="paises-info">
-            <img class = "pais-img" src="${filteredCountries[0].flags.svg}" alt="">
-            <h3>${filteredCountries[0].name.common}</h3>
+            <img class = "pais-img" src="${index.flags.svg}" alt="">
+            <h3>${index.name.common}</h3>
+            </div>
+            `
+        });
+    }
+    if (filteredCountries.length === 1) {
+        const lat = filteredCountries[0].latlng[0];  
+        const lon = filteredCountries[0].latlng[1];
+        getWeather(lat,lon);
+        informacion.innerHTML =  `
+        <div class="paises-info">
+        <img class = "pais-img" src="${filteredCountries[0].flags.svg}" alt="">
+        <h3>${filteredCountries[0].name.common}</h3>
                 <div class="informacion">
                 <p><span>Poblacion:</span> ${filteredCountries[0].population}</p>
                 <p><span>Capital:</span> ${filteredCountries[0].capital}</p>
                 <p><span>Region:</span> ${filteredCountries[0].region}</p>
-                <p><span>Clima:</span> Soleado</p>
+                <p><span>Clima:</span> ${lat}</p>
                 </div>
             </div>
             `
